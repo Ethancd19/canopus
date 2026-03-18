a3ece626440afe6354cd17a5b9c908e8936f9081
ethancd19 <eetyweety@gmail.com>
2026-03-18 05:56:54 -0400

    feat: add exif fields to photo schema

---
+++ prisma/migrations/20260318095621_add_exif_fields/migration.sql
@@ -0,0 +1,9 @@
+-- AlterTable
+ALTER TABLE "Photo" ADD COLUMN     "aperture" TEXT,
+ADD COLUMN     "camera" TEXT,
+ADD COLUMN     "filmFormat" TEXT,
+ADD COLUMN     "filmStock" TEXT,
+ADD COLUMN     "focalLength" TEXT,
+ADD COLUMN     "iso" TEXT,
+ADD COLUMN     "lens" TEXT,
+ADD COLUMN     "shutterSpeed" TEXT;
